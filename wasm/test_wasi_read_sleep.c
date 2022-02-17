2bb140a391c9168af2ae135e2ae099ca31ce0ec4
Kamil Wcisło <mek.xgt@gmail.com>
2022-02-17 01:37:41 +0100

    wasm: add test_wasi_read_sleep app to test multithread capabilities

---
+++ wasm/test_wasi_read_sleep.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main (void)
+{
+    ssize_t ret;
+    int f;
+    char buf[100];
+
+
+    for (int i = 0; ;i++) {
+
+        sprintf(buf, "%5d: ", i);
+        write(STDOUT_FILENO, buf, 7);
+
+        f = open("rom/file", O_RDONLY);
+        if (f < 0) {
+            perror("Open");
+            return 1;
+        }
+
+        ret = read(f, buf, 100);
+        if (ret < 0) {
+            perror("Read");
+            return 1;
+        }
+
+        close(f);
+
+        ret = write(STDOUT_FILENO, buf, ret);
+        if (ret < 0) {
+            perror("Write");
+            return 1;
+        }
+
+        sleep(3);
+    }
+
+    return 0;
+}
