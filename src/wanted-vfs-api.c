0f3fc1f639a4ab29e93326c6140b88645529d39c
Kamil Wcisło <mek.xgt@gmail.com>
2022-03-31 04:51:35 +0200

    wanted-vfs-api: switch to microtojson for generating json values, still not working fully

--- src/wanted-vfs-api.c
+++ src/wanted-vfs-api.c
@@ -4,6 +4,9 @@
 #include <wanted-vfs-api.h>
 #include <platform.h>
 
+#include <wanted_malloc.h>
+#include <mtojson.h>
+
 static wantedConfig_t currentConfig;
 
 #define SUBTRACT_OR_ZERO(a,b) ((a) > (b) ? (a) - (b) : 0)
@@ -11,76 +14,68 @@ static wantedConfig_t currentConfig;
 
 static size_t ConfigToJson(const wantedConfig_t *cfg, uint8_t *buf, size_t bufLen)
 {
-    int pos;
-    size_t left = bufLen;
+    size_t len = cfg->nWapps;
 
-    pos = snprintf(buf, bufLen,
-        "{"
-        "\"wapps\":["
-    );
+    struct to_json *wappsArray = WantedMalloc((len+1) * sizeof(*wappsArray));
+    memset(wappsArray, 0, sizeof(*wappsArray) * (len+1));
 
-    buf += pos;
-    left = SUBTRACT_OR_ZERO(left, pos);
-
-    for (int i = 0; i < cfg->nWapps && left > 0; i++) {
-        pos = snprintf(buf, left, "\"%s\"", cfg->wappsToRun[i]);
-
-        if (pos >= left) break;
-
-        buf += pos;
-        left = SUBTRACT_OR_ZERO(left, pos);
-
-        if (i != cfg->nWapps-1) {
-            pos = snprintf(buf, left, ",");
-            buf += pos;
-            left = SUBTRACT_OR_ZERO(left, pos);
-        }
+    for (int i = 0; i < len; i++) {
+        wappsArray[i].value = cfg->wappsToRun[i];
+        wappsArray[i].vtype = t_to_string;
     }
 
-    if (left == 0) return bufLen;
+    const struct to_json json[] = {
+        {
+            .name = "wapps",
+            .value = wappsArray,
+            .vtype = t_to_array,
+            .stype = t_to_object
+        },
+        { NULL }
+    };
 
-    pos = snprintf(buf, left, "]}\n");
+    len = json_generate(buf, json, bufLen);
 
-    left = SUBTRACT_OR_ZERO(left, pos);
+    WantedFree(wappsArray);
 
-    return bufLen - left;
+    return len;
 }
 
 static size_t RegistryToJson(const reg_entry_t *reg, size_t regLen, uint8_t *buf, size_t bufLen)
 {
-    int pos;
-    size_t left = bufLen;
+    size_t len = regLen;
+    size_t subElemLen = 3;
 
-    pos = snprintf(buf, bufLen,
-        "{"
-        "\"wapps\":["
-    );
+    struct to_json *wappsArray = WantedMalloc((subElemLen * len) * sizeof(*wappsArray));
+    memset(wappsArray, 0, sizeof(*wappsArray) * (subElemLen * len));
 
-    buf += pos;
-    left = SUBTRACT_OR_ZERO(left, pos);
-
-    for (int i = 0; i < regLen && left > 0; i++) {
-        pos = snprintf(buf, left, "{\"name\":\"%s\",\"size\":%d}", reg[i].name, reg[i].size);
-
-        if (pos >= left) break;
-
-        buf += pos;
-        left = SUBTRACT_OR_ZERO(left, pos);
-
-        if (i != regLen - 1) {
-            pos = snprintf(buf, left, ",");
-            buf += pos;
-            left = SUBTRACT_OR_ZERO(left, pos);
-        }
+    wappsArray[0].stype = t_to_array;
+    for (int i = 0; i < len; i++) {
+        wappsArray[i*subElemLen].name = "name";
+        wappsArray[i*subElemLen].value = reg[i].name;
+        wappsArray[i*subElemLen].vtype = t_to_string;
+        wappsArray[i*subElemLen+1].name = "size";
+        wappsArray[i*subElemLen+1].value = &reg[i].size;
+        wappsArray[i*subElemLen+1].vtype = t_to_int;
     }
 
-    if (left == 0) return bufLen;
+    size_t a = 3;
+    const struct to_json json[] = {
+        {
+            .name = "wapps",
+            .value = wappsArray,
+            .count = &a,
+            .vtype = t_to_object,
+            .stype = t_to_object
+        },
+        { NULL }
+    };
 
-    pos = snprintf(buf, left, "]}\n");
+    len = json_generate(buf, json, bufLen);
 
-    left = SUBTRACT_OR_ZERO(left, pos);
+    WantedFree(wappsArray);
 
-    return bufLen - left;
+    return len;
 }
 
 const char *statusToString(status_t state)
@@ -96,47 +91,49 @@ const char *statusToString(status_t state)
 
 static size_t StateToJson(const wapp_state_t *stateList, size_t stateLen, uint8_t *buf, size_t bufLen)
 {
-    int pos;
-    size_t left = bufLen;
+    size_t len = stateLen;
+    size_t subElemLen = 4;
 
-    pos = snprintf(buf, bufLen,
-        "{"
-        "\"wapps\":["
-    );
+    struct to_json *wappsArray = WantedMalloc((subElemLen * len) * sizeof(*wappsArray));
+    memset(wappsArray, 0, sizeof(*wappsArray) * (subElemLen * len));
 
-    buf += pos;
-    left = SUBTRACT_OR_ZERO(left, pos);
-
-    for (int i = 0; i < stateLen && left > 0; i++) {
-        pos = snprintf(buf, left, "{\"name\":\"%s\",\"status\":\"%s\",\"version\":\"%d.%d.%d\"}",
-            stateList[i].name,
-            statusToString(stateList[i].status),
-            stateList[i].version.major, stateList[i].version.minor, stateList[i].version.patch);
-
-        if (pos >= left) break;
-
-        buf += pos;
-        left = SUBTRACT_OR_ZERO(left, pos);
-
-        if (i != stateLen - 1) {
-            pos = snprintf(buf, left, ",");
-            buf += pos;
-            left = SUBTRACT_OR_ZERO(left, pos);
+    wappsArray[0].stype = t_to_array;
+    for (int j = 0; j < subElemLen; j++)
+        for (int i = 0; i < len; i++) {
+            wappsArray[i*3].name    = "name";
+            wappsArray[i*3].value   = stateList[i].name;
+            wappsArray[i*3].vtype   = t_to_string;
+            wappsArray[i*3+1].name  = "status";
+            wappsArray[i*3+1].value = statusToString(stateList[i].status);
+            wappsArray[i*3+1].vtype = t_to_string;
+            wappsArray[i*3+2].name  = "version";
+            wappsArray[i*3+2].value = stateList[i].version.v;
+            wappsArray[i*3+2].vtype = t_to_hex_u32;
         }
-    }
 
-    if (left == 0) return bufLen;
+    const struct to_json json[] = {
+        {
+            .name = "wapps",
+            .value = wappsArray,
+            .count = &len,
+            .vtype = t_to_object,
+            .stype = t_to_object
+        },
+        { NULL }
+    };
 
-    pos = snprintf(buf, left, "]}\n");
+    len = json_generate(buf, json, bufLen);
 
-    left = SUBTRACT_OR_ZERO(left, pos);
+    WantedFree(wappsArray);
 
-    return bufLen - left;
+    return len;
 }
 
 int WantedSetConfig(wantedConfig_t cfg)
 {
     currentConfig = cfg;
+
+    return 0;
 }
 
 int WantedGetConfig(uint8_t *buf, size_t bufLen)
@@ -153,6 +150,7 @@ int WantedReadRegistry(uint8_t *buf, size_t bufLen)
     if (buf == NULL) return -1;
 
     n = PlatformReadRegistry(entries, MAX_WAPPS);
+    if (n < 0) return n;
 
     return RegistryToJson((const reg_entry_t *)&entries, n, buf, bufLen);
 }
@@ -162,6 +160,7 @@ int WantedReadState(uint8_t *buf, size_t bufLen)
     wapp_state_t wapps[MAX_WAPPS];
 
     int ret = GetState(wapps, MAX_WAPPS);
+    if (ret < 0) return ret;
 
     return StateToJson(wapps, ret, buf, bufLen);
 }
--- test/test-wanted-vfs-api.c
+++ test/test-wanted-vfs-api.c
@@ -36,18 +36,58 @@ TEST(wanted_vfs_api, WantedGetConfigTest)
     int ret;
     uint8_t buf[100];
     json_t m[32];
+    wantedConfig_t cfg = {
+        .nWapps = 3,
+        .wappsToRun = { "aa", "bb", "cc", },
+    };
+
+    ret = WantedSetConfig(cfg);
+    TEST_ASSERT_EQUAL(0, ret);
 
     ret = WantedGetConfig(buf, 100);
+    TEST_ASSERT_EQUAL(26, ret);
 
-    TEST_ASSERT_EQUAL(13, ret);
-
+    /* check that the json is valid */
     json_t const* json = json_create(buf, m, sizeof m / sizeof *m);
-
     TEST_ASSERT_NOT_NULL(json);
+
+    json_t const* wapps = json_getProperty(json, "wapps");
+    if (!wapps || JSON_ARRAY != json_getType(wapps)) {
+        TEST_FAIL_MESSAGE("Error, the wapps not found.");
+    }
+
+    json_t const* wapp;
+    int i;
+    for (i = 0, wapp = json_getChild(wapps); wapp != 0 && i < cfg.nWapps; wapp = json_getSibling(wapp), i++) {
+        if (JSON_TEXT == json_getType(wapp)) {
+           TEST_ASSERT_EQUAL_STRING(cfg.wappsToRun[i], json_getValue(wapp));
+        }
+    }
+}
+
+TEST(wanted_vfs_api, WantedReadRegistryTest)
+{
+    int ret;
+    uint8_t buf[100];
+    json_t m[32];
+
+    ret = WantedReadRegistry(buf, 100);
+    //TEST_ASSERT_EQUAL(26, ret);
+
+    return ;
+    /* check that the json is valid */
+    json_t const* json = json_create(buf, m, sizeof m / sizeof *m);
+    TEST_ASSERT_NOT_NULL(json);
+
+    json_t const* wapps = json_getProperty(json, "wapps");
+    if (!wapps || JSON_ARRAY != json_getType(wapps)) {
+        TEST_FAIL_MESSAGE("Error, the wapps not found.");
+    }
 
 }
 
 TEST_GROUP_RUNNER(wanted_vfs_api)
 {
     RUN_TEST_CASE(wanted_vfs_api, WantedGetConfigTest);
+    RUN_TEST_CASE(wanted_vfs_api, WantedReadRegistryTest);
 }
