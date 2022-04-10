a9023d98779475d7bd24a16061d54331fa743d60
Kamil Wcisło <mek.xgt@gmail.com>
2022-04-11 01:04:27 +0200

    platform/linux/random: use stdlib functions

--- platform/linux/api/random.c
+++ platform/linux/api/random.c
@@ -1,14 +1,19 @@
-#include <sys/random.h>
+#include <stdlib.h>
+#include <time.h>
 #include <errno.h>
+#include <limits.h>
 
 #include <platform.h>
 
 int64_t PlatfromGetRandom(uint8_t *buf, size_t buf_len)
 {
-    int64_t ret = getrandom(buf, buf_len, 0);
-    if (ret < 0) {
-        return -errno;
+    if (buf == NULL) return -EINVAL;
+
+    srandom((unsigned int)time(NULL));
+
+    for (size_t i = 0; i < buf_len; i++) {
+        buf[i] = rand() % UCHAR_MAX;
     }
 
-    return ret;
+    return 0;
 }
