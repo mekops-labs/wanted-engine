a066039213844145160942d886bc0039f53f9b55
Kamil Wcisło <mek.xgt@gmail.com>
2023-12-11 23:01:53 +0100

    platform/esp-idf/main: implement rudimentary start point

--- platform/esp-idf/main/main.c
+++ platform/esp-idf/main/main.c
@@ -8,65 +8,43 @@
 #include "esp_system.h"
 
 #include <stdio.h>
+#include <string.h>
 #include <time.h>
 #include <unistd.h>
-#include <c9.h>
+#include <errno.h>
 
-#include "wasm3.h"
-#include "m3_env.h"
+#include <wanted.h>
 
-#include "m3_api_esp_wasi.h"
-#include "wasi_test.wasm.h"
 
 #define FATAL(msg, ...) { printf("Fatal: " msg "\n", ##__VA_ARGS__); return; }
+#define STR(...) #__VA_ARGS__
 
-static void run_wasm(void)
+char* cfg = STR(
 {
-    M3Result result = m3Err_none;
-
-    uint8_t* wasm = (uint8_t*)wasi_test_wasm;
-    uint32_t fsize = wasi_test_wasm_len;
-
-    printf("Loading WebAssembly...\n");
-    IM3Environment env = m3_NewEnvironment ();
-    if (!env) FATAL("m3_NewEnvironment failed");
-
-    IM3Runtime runtime = m3_NewRuntime (env, 8*1024, NULL);
-    if (!runtime) FATAL("m3_NewRuntime failed");
-
-    IM3Module module;
-    result = m3_ParseModule (env, &module, wasm, fsize);
-    if (result) FATAL("m3_ParseModule: %s", result);
-
-    result = m3_LoadModule (runtime, module);
-    if (result) FATAL("m3_LoadModule: %s", result);
-
-    result = m3_LinkEspWASI (runtime->modules);
-    if (result) FATAL("m3_LinkEspWASI: %s", result);
-
-    IM3Function f;
-    result = m3_FindFunction (&f, runtime, "_start");
-    if (result) FATAL("m3_FindFunction: %s", result);
-
-    printf("Running...\n");
-
-    const char* i_argv[2] = { "test.wasm", NULL };
-
-    m3_wasi_context_t* wasi_ctx = m3_GetWasiContext();
-    wasi_ctx->argc = 1;
-    wasi_ctx->argv = i_argv;
-
-    result = m3_CallV (f);
-
-    if (result) FATAL("m3_Call: %s", result);
+    "system": {
+        "defaultWapps": [
+            "a",
+            "bb",
+            "ccc"
+        ]
+    }
 }
+);
+
 
 void app_main(void)
 {
-    printf("\nWasm3 v" M3_VERSION " on " CONFIG_IDF_TARGET ", build " __DATE__ " " __TIME__ "\n");
+    printf("\nWanted on " CONFIG_IDF_TARGET ", build " __DATE__ " " __TIME__ "\n");
 
     clock_t start = clock();
-    run_wasm();
+    int ret = WantedStart(cfg, strlen(cfg));
+    if (ret < 0) {
+        errno = -ret;
+        perror("wanted error");
+    }
+
+    printf("\nAll wapps ended, done...\n");
+
     clock_t end = clock();
 
     printf("Elapsed: %ld ms\n", (end - start)*1000 / CLOCKS_PER_SEC);
